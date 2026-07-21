0e6345259ab0c37de3fe70dc3b1242a2611551ba
mandyking308-hub <mandyking308@gmail.com>
2026-07-21 12:08:15 +0100

    Apply public website design system

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
-import { CookieConsentBanner } from "@/components/gdpr/CookieConsentBanner";
 import { CookieBanner } from "@/components/CookieBanner";
 import { Analytics } from "@/components/Analytics";
 import { ContentProtection } from "@/components/ContentProtection";
+import "@/styles/ghat-public.css";
 
 export const Layout = () => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="ghat-public-shell min-h-screen flex flex-col">
       <Header />
       <main id="main-content" className="flex-grow">
         <Outlet />
