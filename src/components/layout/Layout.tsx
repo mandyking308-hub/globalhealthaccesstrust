41c15731e40fc95b52037cbc6d94a63dcf4ec6d7
mandyking308-hub <mandyking308@gmail.com>
2026-07-24 11:56:07 +0100

    Add workstream navigation and homepage project cards

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { WorkNavigationStrip } from "./WorkNavigationStrip";
 import { CookieBanner } from "@/components/CookieBanner";
 import { Analytics } from "@/components/Analytics";
 import { ContentProtection } from "@/components/ContentProtection";
+import { HomepageWorkstreamsSection } from "@/components/workstreams/HomepageWorkstreamsSection";
+import { WorkstreamFormContext } from "@/components/workstreams/WorkstreamFormContext";
 
 export const Layout = () => {
+  const location = useLocation();
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
+      <WorkNavigationStrip />
+      <WorkstreamFormContext />
       <main id="main-content" className="flex-grow">
         <Outlet />
+        {location.pathname === "/" && <HomepageWorkstreamsSection />}
       </main>
       <Footer />
       <CookieBanner />
