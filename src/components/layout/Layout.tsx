8964bb5847515ce7e6f3939b7724fd646f2926a7
mandyking308-hub <mandyking308@gmail.com>
2026-07-24 11:28:14 +0100

    Show current workstreams on homepage

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,16 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { CookieBanner } from "@/components/CookieBanner";
 import { Analytics } from "@/components/Analytics";
 import { ContentProtection } from "@/components/ContentProtection";
+import { HomepageWorkstreamsSection } from "@/components/workstreams/HomepageWorkstreamsSection";
 
 export const Layout = () => {
+  const location = useLocation();
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main id="main-content" className="flex-grow">
         <Outlet />
+        {location.pathname === "/" && <HomepageWorkstreamsSection />}
       </main>
       <Footer />
       <CookieBanner />
